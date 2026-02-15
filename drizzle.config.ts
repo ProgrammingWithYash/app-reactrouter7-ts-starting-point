675ec6cdb45f5d4d011d330eca19742ce6ae3aeb
Yash Vardhan Kejriwal <74137771+ProgrammingWithYash@users.noreply.github.com>
2026-02-16 01:04:40 +0530

    Update drizzle conf

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,32 @@
 import { defineConfig } from "drizzle-kit";
 
-export default defineConfig({
-	schema: "./app/db/schema/schema.ts",
-	out: "./drizzle",
-	dialect: "sqlite",
-	dbCredentials: {
-		url: "./app.db"
+const DB_TYPE = process.env.DB_TYPE || "sqlite";
+
+const configs = {
+	sqlite: {
+		schema: "./app/db/schema/schema.ts",
+		out: "./drizzle",
+		dialect: "sqlite" as const,
+		dbCredentials: {
+			url: process.env.DB_URL || "./app.db"
+		}
+	},
+	mysql: {
+		schema: "./app/db/schema/schema-mysql.ts",
+		out: "./drizzle",
+		dialect: "mysql" as const,
+		dbCredentials: {
+			url: process.env.DB_URL!
+		}
+	},
+	postgres: {
+		schema: "./app/db/schema/schema-postgres.ts",
+		out: "./drizzle",
+		dialect: "postgresql" as const,
+		dbCredentials: {
+			url: process.env.DB_URL!
+		}
 	}
-});
+};
+
+export default defineConfig(configs[DB_TYPE as keyof typeof configs]);
